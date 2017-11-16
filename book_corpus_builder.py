e8a87700ac13111a6c2e8ac4e5b8776dc4b50ae7
cheriexu <cxuhan@stanford.edu>
2017-11-16 00:09:26 -0800

    put in sleep

--- book_corpus_builder.py
+++ book_corpus_builder.py
@@ -1,8 +1,9 @@
 import json, requests
 from datetime import timedelta, date
 from sets import Set
+from time import sleep
 
-text_file = open("book-corpus.txt", "w")
+text_file = open("book-corpus1.txt", "w")
 
 url = 'https://api.nytimes.com/svc/books/v3/lists.json'
 
@@ -11,18 +12,15 @@ isbns = set()
 def daterange(start_date, end_date):
 	for n in range(int ((start_date - end_date).days)):
 		yield start_date - timedelta(n)
-
+booktypes = ["combined-print-and-e-book-fiction", "advice-how-to-and-miscellaneous", "childrens-middle-grade-e-book", "childrens-middle-grade-hardcover", "childrens-middle-grade-paperback","picture-books","series-books","young-adult-e-book","young-adult-hardcover","young-adult-paperback" ]
 start_date = date(2017, 11, 15)
 #end_date = date(2017, 11, 12)
 end_date = date(2008, 1, 1)
 #end_date = date(2016, 11, 15)
 for single_date in daterange(start_date, end_date):
 	dateParam = single_date.strftime("%Y-%m-%d")
-	for i in range(0, 2):
-		if i == 0:
-			bookType = "hardcover-fiction"
-		else:
-			bookType = "hardcover-nonfiction"
+	for i in range(len(booktypes)):
+		booktype = booktypes[i]
 		params = {
 			"api_key" : '0567a6ed99cf46ed8b113b0553378413',
 			"date" : dateParam,
@@ -32,6 +30,7 @@ for single_date in daterange(start_date, end_date):
 		try:
 			resp = requests.get(url=url, params=params)
 			data = json.loads(resp.text)
+            sleep(.2)
 		except ValueError:
 			print("oh no, value error!")
 			continue
