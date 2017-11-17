9ce1fc9528a850adbe04d028697f4d2bd09e3c14
cheriexu <cxuhan@stanford.edu>
2017-11-17 17:37:38 -0500

    add nlp features and add in mlpregressor, need to convert to arrays

--- variable-corpus-reader.py
+++ variable-corpus-reader.py
@@ -1,4 +1,5 @@
-import ast 
+import ast, re, random
+from sklearn.neural_network import MLPRegressor
 
 #Converts a text file to a python list of default dictionairies representing books
 
@@ -15,21 +16,42 @@ dataList =  ['{' + e + '}' for e in data.split(d)] #List of strings representing
 bVariables = [] #List of dicts representing sparce vectors representing books
 
 for book in dataList:
-	bVariables.append(ast.literal_eval(dataList[4]))
+	#Create nlp features, right now individual words
+	bookdict = ast.literal_eval(book)
+	for key in bookdict.keys():
+		if re.match('rank.*',key): bookdict[key] = 0 #zeroing out rank data
+		if re.match('description.*',key):
+			words = key.split(" ")
+			for w in words[2:]:
+				if w != '':
+					bookdict[w] = 1
+	bVariables.append(bookdict)
 
 myfile.close()
-
 
 
 with open('p-variables-corpus.txt', 'r') as myfile:
     data = myfile.read()
 
 pVariables = ast.literal_eval(data) #List of variables representing the P variables
-
+#create training and testing
 myfile.close()
+trainingB = []
+trainingP = []
+testingB = []
+testingP = []
+for i in xrange(len(bVariables)):
+	if random.random() < 0.1:
+		testingB.append(bVariables[i])
+		testingP.append(pVariables[i])
+	else:
+		trainingB.append(bVariables[i])
+		trainingP.append(pVariables[i])
 
-
-
+#TODO: Reshape testingB and trainingb data into 2D ARRAY size n x m where n is the number of samples and m is the num of features
+#Consider all unqiue features and ndarray()?
 #HERE:
-#use pVariables and bVariables in the regression. Unfortunately bVariables in not a
-#defaultdict, its a regular dict. 
+nn = MLPRegressor(hidden_layer_sizes = (10,10),activation = 'tanh', solver = 'sgd',\
+ batch_size= 'auto',learning_rate='constant', learning_rate_init =0.001,max_iter=200) #adjust parameters if needed, espeically layers, use heuristics in write up
+training = nn.fit(trainingB,trainingP)
+print nn.score(trainingB,trainingP)
