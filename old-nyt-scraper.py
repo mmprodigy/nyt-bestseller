507497f094d0b04f70011967b6a5e4cc906e17fe
cheriexu <cxuhan@stanford.edu>
2017-12-04 18:48:54 -0800

    scraped old NYT before 2008 for xml data, stored under oldnytGR_output.txt

--- old-nyt-scraper.py
+++ old-nyt-scraper.py
@@ -3,37 +3,37 @@ import requests
 from time import sleep
 
 
-
+file = open("oldnytGR_output.txt", "a")
 with open('old_nyt_corpus.txt', 'r') as myfile:
 	data = myfile.read()
-
+delimitor  = "!!!this is a delimitor!!"
 books = ast.literal_eval(data)
 
 key = 'LZH0ts76EISqgfIICORHPA'
 url =  'https://www.goodreads.com/book/title.xml'
-
+i = 0
 
 for book in books:
 	title = book[0]
 	author = book[1]
-	
 	params = {
 				"format" : "xml",
 				"key" : key,
 				"title" : title,
 				"author" : author
 			}
-
 	try:
 		resp = requests.get(url=url, params=params)
 	except:
 		print("OH NO! COULDN'T GET REQUEST!")
 		raise
+	i+=1
+	file.write(resp.content)
+	file.write(delimitor)
+	print str(i) + "books scraped"
 
-	bookXml = resp.content #XML OF BOOK
-	print bookXml
 	sleep(1)
-
+file.close()
 	
 
 
